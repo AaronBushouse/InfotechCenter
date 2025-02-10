cf1894a458ff2c3126abf78b1aee6f30ced1bdaf
AaronBushouse <abushouse2@gmail.com>
2025-02-10 14:10:59 -0500

    comments on weather brach- STABLE

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,20 +1,25 @@
+# Print a header to introduce the program
 print("\n******************************************\n")
- 
 print("Weather Branch - Developer: Aaron Bushouse\n\n\n")
- 
-#Import Libraries HERE!
-import random
-from time import sleep
- 
-# Weather Function to determine the weather
-def weather():
-    weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
-    weatherCondition = random.choice(weatherForecastList)
-    return weatherCondition
 
+# Import necessary libraries
+import random  # For selecting a random weather condition
+from time import sleep  # (Not used in this script but imported)
+
+# Function to determine the weather
+def weather():
+    # List of possible weather conditions
+    weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
+    # Randomly choose one weather condition from the list
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition  # Return the chosen weather condition
+
+# Generate a weather condition and store it in weatherAlert
 weatherAlert = weather()
 
+# Function to determine vehicle response based on weather conditions
 def vehicleResponseSystem():
+    # Check the weather condition and update the alarm accordingly
     if weatherAlert == "snowing":
         print("\n\nThe National Weather Service has updated our alarm by 30 minutes because"
               " it is", weatherAlert,"\n\n")
@@ -26,10 +31,13 @@ def vehicleResponseSystem():
               " it is a", weatherAlert,"drive safe!\n\n")
     elif weatherAlert == "rainy":
         print("\n\nThe National Weather Service has updated our alarm by 10 minutes because"
-              " it is a", weatherAlert,", possible low visiblity\n\n") 
+              " it is a", weatherAlert,", possible low visibility\n\n") 
     elif weatherAlert == "windy":
         print("\n\nThe National Weather Service has updated our alarm by 5 minutes because"
               " it is a", weatherAlert,"\n\n")
     else:
-        print("\nThe National Weather Service is calling for",weatherAlert,"skys! Enjoy it!\n\n")
+        # Default message for sunny weather
+        print("\nThe National Weather Service is calling for",weatherAlert,"skies! Enjoy it!\n\n")
+
+# Call the function to display the response based on the generated weather
 vehicleResponseSystem()
