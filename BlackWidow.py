1f569963e2691ce8c3897acc65f1dff16ff8d2e1
AaronBushouse <abushouse2@gmail.com>
2025-02-17 13:52:55 -0500

    tests prints gone and added Gas level alerts -STABLE

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,4 @@
-print("\n*********************************************************")
+print("\n***************************************************\n")
 print("Gasoline Branch - Developer: Aaron Bushouse\n")
  
 import random
@@ -9,8 +9,24 @@ def gasLevelGauge():
     return random.choice(gasLevelList)
  
 def gasStations():
-    gasStationsList = ["Shell", "Marathon", "Speedway", "Circle K", "Wesco", "Meijer", "Bucees"]
+    gasStationsList = ["Shell", "Marathon", "Speedway", "Circle K", "Wesco", "Meijer", "Buc-ees"]
     return random.choice(gasStationsList)
  
-print(gasLevelGauge())
-print(gasStations())
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(.1,25),1)
+    milesToGasStationQuarterTank = round(random.uniform(25.001,50),1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty":
+        print("*****WARNING - YOU ARE OUT OF GAS*****\n")
+        sleep(1.225)
+        print("calling AAA")
+    elif gasLevelIndicator == "Low":
+        print("Your gas tank is really low, checking GPS for closest gas station")
+        sleep(1.225)
+        print("the closest gas station is", gasStations(), "which is", milesToGasStationLow, "miles away")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("Your gas tank is on a quater tank, checking GPS for closest gas station")
+        sleep(1.225)
+        print("the closest gas station is", gasStations(), "which is", milesToGasStationQuarterTank, "miles away")
+        
+gasLevelAlert()
