3add55c868d5585757c361a2c3299016864e724b
AaronBushouse <abushouse2@gmail.com>
2025-02-17 14:08:49 -0500

    All is stable- STABLE

--- BlackWidow.py
+++ BlackWidow.py
@@ -22,21 +22,19 @@ def gasLevelAlert():
         sleep(1.225)
         print("calling AAA")
         sleep(2)
-        print("AAA is" ,AaaMilesFromYou, "miles away from you sit")
+        print("AAA is" ,AaaMilesFromYou, "miles away from you sit still.")
     elif gasLevelIndicator == "Low":
-        print("Your gas tank is really low, checking GPS for closest gas station")
+        print("Your gas tank is really low, checking GPS for closest gas station.")
         sleep(1.225)
         print("the closest gas station is", gasStations(), "which is", milesToGasStationLow, "miles away")
     elif gasLevelIndicator == "Quarter Tank":
-        print("Your gas tank is on a quater tank, checking GPS for closest gas station")
+        print("Your gas tank is on a quater tank, checking GPS for closest gas station.")
         sleep(1.225)
         print("the closest gas station is", gasStations(), "which is", milesToGasStationQuarterTank, "miles away")
-    elif gasLevelIndicator == "Half Tank"
-        print("you have plenty of gas to get to you destination")
-    elif gasLevelIndicator == "Half Tank"
-        print("your gas tank is at half, you have plenty of gas to get to you destination")
-    elif gasLevelIndicator == "Three Quarter Tank"
-        print("your gas tank is at three quaters, you have plenty of gas to get to you destination")
+    elif gasLevelIndicator == "Half Tank":
+        print("your gas tank is at half, you have plenty of gas to get to you destination.")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("your gas tank is at three quaters, you have plenty of gas to get to you destination.")
     else:
         print("you have a full tank have a safe drive!")
         
